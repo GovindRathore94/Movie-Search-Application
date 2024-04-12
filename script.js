fe41532c5f28c7cf9dd544f647bcc4d5c39687e5
Govind Singh Rathore <146634853+GovindRathore94@users.noreply.github.com>
2024-04-12 16:14:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+
+const accessKey = "V87MrDeoroBbR6K8KzG7xkcTROJbyCC01lMS42F8gOg";
+
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMoreBtn = document.getElementById("show-more-btn");
+
+let keyword = "";
+let page = 1;
+
+async function searchImage(){
+    keyword = searchBox.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if(page ===1){
+        searchResult.innerHTML = "";
+    }
+
+    const results = data.results
+
+    // console.log(data);
+    // console.log(results);
+
+    results.map((result)=>{
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+
+        imageLink.appendChild(image);
+
+        searchResult.appendChild(imageLink);
+    })
+    showMoreBtn.style.display = "block";
+}
+
+searchForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    page = 1;
+    searchImage()
+})
+
+showMoreBtn.addEventListener("click", ()=>{
+    page++;
+    searchImage();
+})
