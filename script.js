ac1cdd0b9ea3aa2be3e793aba201500e1bf0bf22
Govind Singh Rathore <146634853+GovindRathore94@users.noreply.github.com>
2024-04-20 11:11:18 +0530

    script.js -> Update https and http Protocol for Network Request Error !!
    Update https and http Protocol for Network Request Error !!

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const inputBox = document.querySelector(".inputBox");
 const getMovieInfo = async (movie) => {
   try {
     const myApiKey = "1d2d3802";
-    const url = `http://www.omdbapi.com/?apikey=${myApiKey}&t=${movie}`;
+    const url = `https://www.omdbapi.com/?apikey=${myApiKey}&t=${movie}`;
 
     const response = await fetch(url);
 
